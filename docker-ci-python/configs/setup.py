0aea0f279d8cddc599e9e17776c4bbcc3a5f7254
Anton Berezin <anton.berezin@nephilim.solutions>
2018-02-12 18:24:33 +0200

    Moved list requirements to be a subcommand of setup.py script

--- docker-ci-python/configs/setup.py
+++ docker-ci-python/configs/setup.py
@@ -8,15 +8,7 @@ from yaml import load
 assert sys.version_info.major == 3, "Only Python 3 is supported"
 
 
-PROJECT_PATH = "/project"
-
-
-def _pt(name):
-    return os.path.join(PROJECT_PATH, name)
-
-
 def _read(name):
-    name = _pt(name)
     if not os.path.exists(name):
         raise ValueError("File does not exist: {}".format(name))
     with open(name) as fil:
@@ -31,10 +23,20 @@ def _get_version():
     return _read("CHANGES").split("\n")[0].split()[0]
 
 
-setup(
-    version=_get_version(),
-    packages=find_packages(exclude=["tests", "integration_tests"]),
-    include_package_data=True,
-    long_description=_read("README.rst"),
-    **_setup_yml()
-)
+def _print_all_requirements():
+    data = _setup_yml()
+    for reqs in ["install_requires", "setup_requires", "tests_require"]:
+        for req in data.get(reqs, []):
+            print(req, end=' ')
+
+
+if sys.argv[1] == "list-requirements":
+    _print_all_requirements()
+else:
+    setup(
+        version=_get_version(),
+        packages=find_packages(exclude=["tests", "integration_tests"]),
+        include_package_data=True,
+        long_description=_read("README.rst"),
+        **_setup_yml()
+    )
