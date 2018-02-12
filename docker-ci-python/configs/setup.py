6dd99de91c46001b854f3da146a1b4108ff94c5a
Anton Berezin <anton.berezin@nephilim.solutions>
2018-02-12 15:03:21 +0200

    Put all requirements to setup.yml

--- docker-ci-python/configs/setup.py
+++ docker-ci-python/configs/setup.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 from yaml import load
 
 
@@ -12,42 +11,30 @@ assert sys.version_info.major == 3, "Only Python 3 is supported"
 PROJECT_PATH = "/project"
 
 
-def _setup_yml():
-    with open(_pt("setup.yml")) as setup_yml:
-        return load(setup_yml.read())
-
-
 def _pt(name):
     return os.path.join(PROJECT_PATH, name)
 
 
 def _read(name):
+    name = _pt(name)
     if not os.path.exists(name):
-        return "<TBD>"
-    with open(_pt(name)) as fil:
+        raise ValueError("File does not exist: {}".format(name))
+    with open(name) as fil:
         return fil.read()
+
+
+def _setup_yml():
+    return load(_read("setup.yml"))
 
 
 def _get_version():
     return _read("CHANGES").split("\n")[0].split()[0]
 
 
-def _get_long_description():
-    return _read("README.rst")
-
-
-def _get_requirements():
-    req_f = _pt("requirements.txt")
-    if os.path.exists(req_f):
-        return [str(ir.req) for ir in parse_requirements(req_f, session=False)]
-    else:
-        return []
-
-
 setup(
-    install_requires=_get_requirements(),
     version=_get_version(),
     packages=find_packages(exclude=["tests", "integration_tests"]),
     include_package_data=True,
+    long_description=_read("README.rst"),
     **_setup_yml()
 )
--- docker-ci-python/docker/install-if-exists.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-[ -e $1 ] && pip install -q -r $1
-true
